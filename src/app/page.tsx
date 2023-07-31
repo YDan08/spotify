02375386fc378cfbc30db1e2f7f2520d6cbd811c
YDan08 <dasyamashita@gmail.com>
2023-07-31 09:56:04 -0300

    feat: adicionar dados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import { ItemPlaylist } from "@/components/ItemPlaylist"
+import { getPlaylists } from "@/utils/playlist.server"
 
-export default function Home() {
+export const Home = async () => {
+	const playlists = await getPlaylists()
+
 	return (
 		<div>
 			<h1 className='text-3xl'>Boa Noite</h1>
 			<div className='mt-10 grid grid-cols-3 gap-8'>
-				<ItemPlaylist />
-				<ItemPlaylist />
-				<ItemPlaylist />
-				<ItemPlaylist />
-				<ItemPlaylist />
-				<ItemPlaylist />
+				{playlists.length !== 0 &&
+					playlists.map(playlist => (
+						<ItemPlaylist key={playlist.id} id={playlist.id} nome={playlist.nome} />
+					))}
 			</div>
 		</div>
 	)
 }
+
+export default Home
--- src/components/ItemPlaylist/ItemPlaylist.tsx
+++ src/components/ItemPlaylist/ItemPlaylist.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image"
+import Link from "next/link"
 
-export const ItemPlaylist = () => {
+interface ItemPLaylistProps {
+	id: number
+	nome: string
+}
+
+export const ItemPlaylist = ({ nome, id }: ItemPLaylistProps) => {
 	return (
-		<div className='rounded flex bg-zinc items-center cursor-pointer hover:bg-zinc-600'>
-			<Image src='/album.jpg' alt='album' width={80} height={80} className='rounded-l' />
-			<h3 className='ml-5'>Blurryface</h3>
-		</div>
+		<Link href={`/playlists/${id}`}>
+			<div className='rounded flex bg-zinc items-center cursor-pointer hover:bg-zinc-600'>
+				<Image src='/album.jpg' alt='album' width={80} height={80} className='rounded-l' />
+				<h3 className='ml-5'>{nome}</h3>
+			</div>
+		</Link>
 	)
 }
 
